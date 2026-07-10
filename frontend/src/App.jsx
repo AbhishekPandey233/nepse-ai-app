ced104f40c53ac152ca7706d3c342b2c019a8591
abhpandey <abhishekpaney54@gmail.com>
2026-07-10 13:21:03 +0545

    refactor routing structure to include Layout component and add ExplainabilityPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
+import Layout from "./components/Layout.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
+import ExplainabilityPage from "./pages/ExplainabilityPage.jsx";
 import Login from "./pages/Login.jsx";
 import PredictionExplorer from "./pages/PredictionExplorer.jsx";
 import VolatilityPage from "./pages/VolatilityPage.jsx";
@@ -10,9 +12,12 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/prediction" element={<PredictionExplorer />} />
-        <Route path="/volatility" element={<VolatilityPage />} />
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/prediction" element={<PredictionExplorer />} />
+          <Route path="/volatility" element={<VolatilityPage />} />
+          <Route path="/explainability" element={<ExplainabilityPage />} />
+        </Route>
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
