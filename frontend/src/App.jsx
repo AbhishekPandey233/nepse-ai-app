642b93a257dfcbe59d46cdde5cf01152236f67da
abhpandey <abhishekpaney54@gmail.com>
2026-07-10 12:54:04 +0545

    add VolatilityPage route to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard.jsx";
 import Login from "./pages/Login.jsx";
 import PredictionExplorer from "./pages/PredictionExplorer.jsx";
+import VolatilityPage from "./pages/VolatilityPage.jsx";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/prediction" element={<PredictionExplorer />} />
+        <Route path="/volatility" element={<VolatilityPage />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
